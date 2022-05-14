117dcde74c030e89c0c52f647796b3a7f46d27d1
Fudan <fudandori@gmail.com>
2022-05-14 10:05:52 +0200

    Server testing

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	value := r.URL.Query()["number"]
+
+	fmt.Fprintf(w, value[0])
+	fmt.Println("Endpoint Hit: homePage")
+}
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello World!")
+	fmt.Println("Hellow World Called")
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/hello", helloWorld)
+	log.Fatal(http.ListenAndServe(":10000", nil))
+}
+
+func main() {
+	handleRequests()
+}
